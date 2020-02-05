056d8f4142b00fd4dbfe84d8f1f1429d5c0c0579
vipnight320 <owata74@gmail.com>
2020-02-05 16:07:43 +0900

    第12回

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,37 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Task;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('tasks');
+});
+
+/**
+ * 新タスク追加
+ */
+Route::post('/task', function (Request $request) {
+    //
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:191',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+});
+
+/**
+ * 既存タスク削除
+ */
+Route::delete('/task/{id}', function ($id) {
+    //
 });
